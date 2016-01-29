05582142d3497ae674d0a3f878f2f348ae6a957e
john sandstedt <john sandstedt>
2016-01-29 14:26:20 -0500

    finnished the code to read a config file for setting username and password

--- config.py
+++ config.py
@@ -18,14 +18,14 @@ class config:
 
     def __init__(self, path=None):
         if not path==None:
-            path = os.path.join(path, 'config.cfg') #custom config dir
+            self.path = os.path.join(path, 'config.cfg') #custom config dir
         else:
-            path = 'config.cfg'
+            self.path = 'config.cfg'
         self.config = ConfigParser.ConfigParser()
-        if not os.path.exists(path):
+        if not os.path.exists(self.path):
             logging.debug("generating config file")
-            self.generate_cfg(path)
-        self.config.readfp(open(path))
+            self.generate_cfg()
+        self.config.readfp(open(self.path))
 
     def get_password(self):
         return(self.config.get("Credentials", "password"))
@@ -36,11 +36,29 @@ class config:
     def get_interval(self):
         return(self.config.get("Settings", "interval")) #has a unit at the end so no int
 
-    def generate_cfg(self, path):
+    def is_auto_generated(self):
+        return(self.config.get("Settings", "run wizard"))
+
+    def set_wizard_ran(self):
+        self.config.set("Settings", "run wizard", '0')
+        self.write_out()
+
+    def set_username(self, name):
+        self.config.set("Credentials", "username", name)
+        self.write_out()
+
+    def set_password(self, password):
+        self.config.set("Credentials", "password", password)
+        self.write_out()
+
+    def write_out(self):
+        self.config.write(open(self.path, "w"))
+
+    def generate_cfg(self):
         self.config.add_section("Credentials")
         self.config.set("Credentials", "username", "blahname")
         self.config.set("Credentials", "password", "blahpass")
         self.config.add_section("Settings")
         self.config.set("Settings", "interval", "1hr")
-        self.config.write(open(path, "w"))
+        self.writeout()
 d = config()
